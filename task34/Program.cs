50c40d0b15f27c328791a8fec41d2cb6f0a6eacf
vkoshkina <koshkina_v@mail.ru>
2023-02-19 10:44:54 +0300

    task34 done

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,10 +1,11 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
 Console.Clear();
 
 Console.WriteLine("Введите количество элементов массива:  ");
-int size = Convert.ToInt32(Console.ReadLine());
+int size = int.Parse(Console.ReadLine()!);
 int[] numbers = new int[size];
 
 DifNumbers(numbers);
@@ -21,17 +22,17 @@ void DifNumbers(int[] numbers)
 
 int count = 0;
 
-for (int x = 0; x < numbers.Length; x++)
+for (int x = 0; x < size; x++)
 {
 if (numbers[x] % 2 == 0)
 count++;
 }
-Console.Write($"-> {count} четных");
+Console.Write($"-> {count} четных числа");
 
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < size; i++)
     {
         Console.Write(numbers[i] + " ");
     }
