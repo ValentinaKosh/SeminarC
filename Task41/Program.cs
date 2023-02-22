9e7243df929920b2d9eebdbcf57d9ec5474c3771
vkoshkina <koshkina_v@mail.ru>
2023-02-22 16:22:36 +0300

    Task41 done

---
+++ Task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2 
+// 1, -7, 567, 89, 223-> 3 
+
+Console.Clear(); 
+
+int[] GetArray(int size, int MinValue, int MaxValue) 
+{ 
+    int[] Result = new int[size]; 
+    for (int i = 0; i < size; i++) 
+    { 
+        Result[i] = new Random().Next(MinValue, MaxValue + 1); 
+    } 
+    return Result; 
+} 
+
+int KolNum(int[] array) 
+{ 
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i] > 0) 
+        { 
+            count++; 
+        } 
+    } 
+    return count; 
+} 
+
+Console.Write("Введите М чисел = "); 
+int size = int.Parse(Console.ReadLine()!); 
+int[] array = GetArray(size, -100, 100);
+Console.Write("[");
+Console.Write(String.Join(", ", array)); 
+Console.Write("]");
+Console.Write($" -> "); 
+Console.WriteLine(String.Join(", ", KolNum(array)));
