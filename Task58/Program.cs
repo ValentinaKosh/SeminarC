8f323d86c6c2791e19b513801b387431b472db67
vkoshkina <koshkina_v@mail.ru>
2023-02-26 15:48:19 +0300

    Task 58 done

---
+++ Task58/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+Console.Write("Введите количество строк матрицы 1 = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов матрицы 1 или строк матрицы 2 = ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов матрицы 2 = ");
+int p = int.Parse(Console.ReadLine()!);
+
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+void MultiplyMatrix(int[,] Martrix1, int[,] Martrix2, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < Martrix1.GetLength(1); k++)
+      {
+        sum += Martrix1[i,k] * Martrix2[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.Clear();
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"матрица 1:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"матрица 2:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\n Произведение матриц:");
+WriteArray(resultMatrix);
